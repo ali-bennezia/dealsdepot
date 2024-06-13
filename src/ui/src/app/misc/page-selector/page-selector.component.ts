3966cdd26691eeb5370a30b9b41cfdcf4944fe35
ali-bennezia <bennezia.ali@gmail.com>
2024-06-13 10:20:00 +0200

    added interface pagination functionalities

--- src/ui/src/app/misc/page-selector/page-selector.component.ts
+++ src/ui/src/app/misc/page-selector/page-selector.component.ts
@@ -8,6 +8,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class PageSelectorComponent {
   @Input()
   page!: number;
+  @Input()
+  totalPages!: number;
+  @Input()
+  hasPrevPage!: boolean;
+  @Input()
+  hasNextPage!: boolean;
   @Output()
   onSelectPage: EventEmitter<number> = new EventEmitter<number>();
 
